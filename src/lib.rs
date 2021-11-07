01f9048a61786e992bc81d00a495a5d311f3d493
Michael Connor Buchan <mikey@blindcomputing.org>
2021-11-07 11:02:51 +0000

    Start work on a ChildStream type.

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,16 @@
 use std::{
+    future::Future,
+    pin::Pin,
     sync::Arc,
+    task::{Context, Poll},
     time::Duration,
 };
 
 use dbus::{
-    nonblock::{Proxy, SyncConnection},
+    nonblock::{MethodReply, Proxy, SyncConnection},
     strings::{BusName, Path},
 };
+use futures_core::stream::Stream;
 
 use atspi_codegen::accessible::OrgA11yAtspiAccessible;
 
@@ -49,7 +53,7 @@ impl Accessible {
 
 pub async fn child_at_index(&self, idx: i32) -> Result<Option<Self>, dbus::Error> {
     let (dest, path) = self.proxy.get_child_at_index(idx).await?;
-    if dest == "org.a11y.atspi.Registry" && &*path == "/org/a11y/atspi/null" {
+    if dest == "org.a11y.atspi.Registry" && path.as_tr() == "/org/a11y/atspi/null" {
         Ok(None)
     } else {
     let conn = Arc::clone(&self.proxy.connection);
@@ -67,5 +71,56 @@ pub async fn child_at_index(&self, idx: i32) -> Result<Option<Self>, dbus::Error
 
     pub async fn description(&self) -> Result<String, dbus::Error> {
         self.proxy.description().await
+    }
+}
+
+pub struct ChildStream<'a> {
+    parent: &'a Accessible,
+    current: i32,
+    total: i32,
+    retry: bool,
+    fut: Option<MethodReply<(String, Path<'static>)>>,
+}
+
+impl Stream for ChildStream<'_> {
+    type Item = Result<Accessible, dbus::Error>;
+
+    fn poll_next(mut self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Option<Self::Item>> {
+        if self.current >= self.total {
+            return Poll::Ready(None);
+        }
+
+        let fut = Pin::new(if let Some(ref mut fut) = self.fut {
+            fut
+        } else {
+            self.fut = Some(self.parent.proxy.get_child_at_index(self.current));
+            self.fut.as_mut().unwrap()
+        });
+
+        let res = match fut.poll(cx) {
+            Poll::Ready(r) => r,
+            Poll::Pending => return Poll::Pending,
+        };
+        if res.is_err() && !self.retry {
+            self.current += 1;
+            self.fut = None;
+        }
+        Poll::Ready(Some(
+            res.map(|(dest, path)| {
+        let conn = Arc::clone(&self.parent.proxy.connection);
+                Accessible::new(dest, path, conn)
+            }),
+        ))
+    }
+
+    fn size_hint(&self) -> (usize, Option<usize>) {
+        (
+            self.total as _,
+            if self.retry {
+                None
+            } else {
+                Some(self.total as _)
+            },
+        )
     }
 }
